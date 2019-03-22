2cf3a3317a175719c3911eba9fce5e1829d19767
Jimmy Zebroski <zebrozkii@gmail.com>
2019-03-22 10:43:51 -0700

    fixed html page to get users birthday, gathers input from user

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import { pingPong } from './super-galactic-calculator';
+import { Birthday } from './super-galactic-calculator';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
+  $('#galactic-age').submit(function(event) {
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
+    let year = parseInt($("input#birthyear").val());
+    let month = parseInt($("input#birthmonth").val());
+    let day = parseInt($("input#birthday").val());
+
     });
   });
 });
--- src/super-galactic-calculator.js
+++ src/super-galactic-calculator.js
@@ -1,15 +1,8 @@
-export function pingPong(goal) {
-  var output = [];
-  for (var i = 1; i <= goal; i++) {
-    if (i % 15 === 0) {
-      output.push("ping-pong");
-    } else if (i % 3 === 0) {
-      output.push("ping");
-    } else if (i % 5 === 0) {
-      output.push("pong");
-    } else  {
-      output.push(i);
-    }
+export class Birthday {
+  constuctor(month, day, year){
+    this.month = month;
+    this.day = day;
+    this.year = year;
   }
-  return output;
+
 }
