b96954c8f738dfd65086f85bcdbb39af1bb6e43d
Jimmy Zebroski <zebrozkii@gmail.com>
2019-03-22 11:20:42 -0700

    my test are failing trying to figure out why

--- spec/super-galactic-calculator-spec.js
+++ spec/super-galactic-calculator-spec.js
@@ -1,6 +1,12 @@
-describe('Triangle', function() {
+import { Birthday } from './../src/super-galactic-calculator.js';
 
-  it('should test whether a Triangle has three sides', function() {
+describe('Birthday', function() {
+  it('should test to make sure constructor is getting right values', function() {
+    var birthday = new Birthday(03,12,2015);
+    expect(birthday.month).toEqual(03);
+    expect(birthday.day).toEqual(12);
+    expect(birthday.year).not.toEqual(2014);
+
     //Test content will go here.
   });
 });
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Birthday } from './super-galactic-calculator';
+ import { Birthday } from './super-galactic-calculator';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
@@ -7,10 +7,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 $(document).ready(function() {
   $('#galactic-age').submit(function(event) {
     event.preventDefault();
-    let year = parseInt($("input#birthyear").val());
-    let month = parseInt($("input#birthmonth").val());
-    let day = parseInt($("input#birthday").val());
-
+    let year = parseInt($("#birthyear").val());
+    let month = parseInt($("#birthmonth").val());
+    let day = parseInt($("#birthday").val());
+    let birthday = new Birthday(month,day,year);
+    console.log(birthday);
     });
   });
-});
